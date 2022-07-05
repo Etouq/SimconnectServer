73d3690aff2ecdb5ea7395a8791ae391db237e96
Mikey Kootstra <mikey.kootstra@gmail.com>
2022-07-05 17:18:38 +0200

    add new events

--- src/SimInterface/setupEvents.cpp
+++ src/SimInterface/setupEvents.cpp
@@ -5,15 +5,20 @@ void SimInterface::setupEvents()
 {
     SimConnect_MapClientEventToSimEvent(d_simConnectHandle, SIM_COMMAND_IDS::SET_COM1_STBY, "COM_STBY_RADIO_SET_HZ");
     SimConnect_MapClientEventToSimEvent(d_simConnectHandle, SIM_COMMAND_IDS::SET_COM2_STBY, "COM2_STBY_RADIO_SET_HZ");
+    SimConnect_MapClientEventToSimEvent(d_simConnectHandle, SIM_COMMAND_IDS::SET_COM3_STBY, "COM3_STBY_RADIO_SET_HZ");
 
     SimConnect_MapClientEventToSimEvent(d_simConnectHandle, SIM_COMMAND_IDS::SET_NAV1_STBY, "NAV1_STBY_SET_HZ");
     SimConnect_MapClientEventToSimEvent(d_simConnectHandle, SIM_COMMAND_IDS::SET_NAV2_STBY, "NAV2_STBY_SET_HZ");
 
     SimConnect_MapClientEventToSimEvent(d_simConnectHandle, SIM_COMMAND_IDS::SWAP_COM1, "COM_STBY_RADIO_SWAP");
     SimConnect_MapClientEventToSimEvent(d_simConnectHandle, SIM_COMMAND_IDS::SWAP_COM2, "COM2_RADIO_SWAP");
+    SimConnect_MapClientEventToSimEvent(d_simConnectHandle, SIM_COMMAND_IDS::SWAP_COM2, "COM3_RADIO_SWAP");
 
     SimConnect_MapClientEventToSimEvent(d_simConnectHandle, SIM_COMMAND_IDS::SWAP_NAV1, "NAV1_RADIO_SWAP");
     SimConnect_MapClientEventToSimEvent(d_simConnectHandle, SIM_COMMAND_IDS::SWAP_NAV2, "NAV2_RADIO_SWAP");
 
     SimConnect_MapClientEventToSimEvent(d_simConnectHandle, SIM_COMMAND_IDS::SET_XPNDR_CODE, "XPNDR_SET");
+
+    SimConnect_MapClientEventToSimEvent(d_simConnectHandle, SIM_COMMAND_IDS::TOGGLE_GPS_DRIVES_NAV1, "TOGGLE_GPS_DRIVES_NAV1");
+    SimConnect_MapClientEventToSimEvent(d_simConnectHandle, SIM_COMMAND_IDS::SET_AP_NAV_SELECTED, "AP_NAV_SELECT_SET");
 }
