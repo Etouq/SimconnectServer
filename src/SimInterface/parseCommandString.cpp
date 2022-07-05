6ed9419776e3e4b87499ba205fd888099d9b4ddb
Mikey Kootstra <mikey.kootstra@gmail.com>
2022-07-05 17:17:55 +0200

    use correct command ids

--- src/SimInterface/parseCommandString.cpp
+++ src/SimInterface/parseCommandString.cpp
@@ -1,30 +1,17 @@
 #include "enums.hpp"
 #include "SimInterface.hpp"
+#include "common/dataIdentifiers.hpp"
 
 #include <QBuffer>
 #include <stdint.h>
 
-
-enum class CommandIds : uint8_t
-{
-    SET_COM1_FREQ,
-    SET_COM2_FREQ,
-    SET_NAV1_FREQ,
-    SET_NAV2_FREQ,
-    SWAP_COM1,
-    SWAP_COM2,
-    SWAP_NAV1,
-    SWAP_NAV2,
-    SET_XPDR_CODE,
-    SET_XPDR_STATE
-};
 
 void SimInterface::parseCommandString(const QByteArray &data)
 {
     QBuffer buffer;
     buffer.buffer() = data;
     buffer.open(QBuffer::ReadOnly);
-    CommandIds commandId = CommandIds::SET_COM1_FREQ;
+    SimCommandId commandId = SimCommandId::SET_COM1_FREQ;
     // emit receivedError("Command string is: " + data.toHex());
 
     while (buffer.bytesAvailable() > 0)
@@ -34,7 +21,7 @@ void SimInterface::parseCommandString(const QByteArray &data)
 
         switch (commandId)
         {
-            case CommandIds::SET_COM1_FREQ:
+            case SimCommandId::SET_COM1_FREQ:
             {
                 uint32_t newFreq = 0;
                 bool swap = false;
@@ -51,7 +38,7 @@ void SimInterface::parseCommandString(const QByteArray &data)
                 if (!swap)
                     break;
             }
-            case CommandIds::SWAP_COM1:
+            case SimCommandId::SWAP_COM1:
                 SimConnect_TransmitClientEvent(d_simConnectHandle,
                                                SIMCONNECT_OBJECT_ID_USER,
                                                SIM_COMMAND_IDS::SWAP_COM1,
@@ -59,7 +46,7 @@ void SimInterface::parseCommandString(const QByteArray &data)
                                                SIMCONNECT_GROUP_PRIORITY_HIGHEST,
                                                SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
                 break;
-            case CommandIds::SET_COM2_FREQ:
+            case SimCommandId::SET_COM2_FREQ:
             {
                 uint32_t newFreq = 0;
                 bool swap = false;
@@ -76,7 +63,7 @@ void SimInterface::parseCommandString(const QByteArray &data)
                 if (!swap)
                     break;
             }
-            case CommandIds::SWAP_COM2:
+            case SimCommandId::SWAP_COM2:
                 SimConnect_TransmitClientEvent(d_simConnectHandle,
                                                SIMCONNECT_OBJECT_ID_USER,
                                                SIM_COMMAND_IDS::SWAP_COM2,
@@ -84,7 +71,32 @@ void SimInterface::parseCommandString(const QByteArray &data)
                                                SIMCONNECT_GROUP_PRIORITY_HIGHEST,
                                                SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
                 break;
-            case CommandIds::SET_NAV1_FREQ:
+            case SimCommandId::SET_COM3_FREQ:
+            {
+                uint32_t newFreq = 0;
+                bool swap = false;
+                buffer.read(reinterpret_cast<char *>(&newFreq), sizeof(newFreq));
+                buffer.read(reinterpret_cast<char *>(&swap), sizeof(swap));
+
+                SimConnect_TransmitClientEvent(d_simConnectHandle,
+                                               SIMCONNECT_OBJECT_ID_USER,
+                                               SIM_COMMAND_IDS::SET_COM3_STBY,
+                                               newFreq,
+                                               SIMCONNECT_GROUP_PRIORITY_HIGHEST,
+                                               SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+
+                if (!swap)
+                    break;
+            }
+            case SimCommandId::SWAP_COM3:
+                SimConnect_TransmitClientEvent(d_simConnectHandle,
+                                               SIMCONNECT_OBJECT_ID_USER,
+                                               SIM_COMMAND_IDS::SWAP_COM3,
+                                               1,
+                                               SIMCONNECT_GROUP_PRIORITY_HIGHEST,
+                                               SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+                break;
+            case SimCommandId::SET_NAV1_FREQ:
             {
                 uint32_t newFreq = 0;
                 bool swap = false;
@@ -101,7 +113,7 @@ void SimInterface::parseCommandString(const QByteArray &data)
                 if (!swap)
                     break;
             }
-            case CommandIds::SWAP_NAV1:
+            case SimCommandId::SWAP_NAV1:
                 SimConnect_TransmitClientEvent(d_simConnectHandle,
                                                SIMCONNECT_OBJECT_ID_USER,
                                                SIM_COMMAND_IDS::SWAP_NAV1,
@@ -109,7 +121,7 @@ void SimInterface::parseCommandString(const QByteArray &data)
                                                SIMCONNECT_GROUP_PRIORITY_HIGHEST,
                                                SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
                 break;
-            case CommandIds::SET_NAV2_FREQ:
+            case SimCommandId::SET_NAV2_FREQ:
             {
                 uint32_t newFreq = 0;
                 bool swap = false;
@@ -126,7 +138,7 @@ void SimInterface::parseCommandString(const QByteArray &data)
                 if (!swap)
                     break;
             }
-            case CommandIds::SWAP_NAV2:
+            case SimCommandId::SWAP_NAV2:
                 SimConnect_TransmitClientEvent(d_simConnectHandle,
                                                SIMCONNECT_OBJECT_ID_USER,
                                                SIM_COMMAND_IDS::SWAP_NAV2,
@@ -134,7 +146,7 @@ void SimInterface::parseCommandString(const QByteArray &data)
                                                SIMCONNECT_GROUP_PRIORITY_HIGHEST,
                                                SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
                 break;
-            case CommandIds::SET_XPDR_CODE:
+            case SimCommandId::SET_XPDR_CODE:
             {
                 uint32_t newCode = 0;
                 buffer.read(reinterpret_cast<char *>(&newCode), sizeof(newCode));
@@ -147,7 +159,7 @@ void SimInterface::parseCommandString(const QByteArray &data)
                                                SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
                 break;
             }
-            case CommandIds::SET_XPDR_STATE:
+            case SimCommandId::SET_XPDR_STATE:
             {
                 uint32_t newState = 0;
                 buffer.read(reinterpret_cast<char *>(&newState), sizeof(newState));
@@ -163,6 +175,45 @@ void SimInterface::parseCommandString(const QByteArray &data)
                                               &newState);
 
                 break;
+            }
+            case SimCommandId::NEXT_CDI_MODE:
+            {
+                if (d_hsiHandler.gpsDrivesNav1())
+                {
+                    // gps goes to nav 1
+                    SimConnect_TransmitClientEvent(d_simConnectHandle,
+                                               SIMCONNECT_OBJECT_ID_USER,
+                                               SIM_COMMAND_IDS::TOGGLE_GPS_DRIVES_NAV1,
+                                               1,
+                                               SIMCONNECT_GROUP_PRIORITY_HIGHEST,
+                                               SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+                    SimConnect_TransmitClientEvent(d_simConnectHandle,
+                                               SIMCONNECT_OBJECT_ID_USER,
+                                               SIM_COMMAND_IDS::SET_AP_NAV_SELECTED,
+                                               1,
+                                               SIMCONNECT_GROUP_PRIORITY_HIGHEST,
+                                               SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+                }
+                else if (d_hsiHandler.autopilotNavSelected() == 1)
+                {
+                    // nav 1 goes to nav 2
+                    SimConnect_TransmitClientEvent(d_simConnectHandle,
+                                               SIMCONNECT_OBJECT_ID_USER,
+                                               SIM_COMMAND_IDS::SET_AP_NAV_SELECTED,
+                                               2,
+                                               SIMCONNECT_GROUP_PRIORITY_HIGHEST,
+                                               SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+                }
+                else
+                {
+                    // nav 2 goes to gps
+                    SimConnect_TransmitClientEvent(d_simConnectHandle,
+                                               SIMCONNECT_OBJECT_ID_USER,
+                                               SIM_COMMAND_IDS::TOGGLE_GPS_DRIVES_NAV1,
+                                               1,
+                                               SIMCONNECT_GROUP_PRIORITY_HIGHEST,
+                                               SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
+                }
             }
         }
     }
