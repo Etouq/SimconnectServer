482ad7e06e351745d3608f252b6ee50ead2d4083
Mikey Kootstra <mikey.kootstra@gmail.com>
2022-08-05 15:50:19 +0200

    cleanup old code

--- src/mainwindow/clearflightplan.cpp
+++
@@ -1,8 +0,0 @@
-#include "mainwindow.hpp"
-#include "common/dataIdentifiers.hpp"
-
-void MainWindow::clearFlightplan()
-{
-    d_connectionHandler.sendDataToClients(QByteArray::fromStdString({ static_cast<char>(DataGroupIdentifier::MFD_DATA),
-                                                  static_cast<char>(MfdIdentifier::CLEAR_FLIGHTPLAN) }));
-}
