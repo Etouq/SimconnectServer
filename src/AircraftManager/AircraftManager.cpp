1aa6793c4c412188b470a2dee308d7bd035b1aa5
Mikey Kootstra <mikey.kootstra@gmail.com>
2022-09-09 13:30:54 +0200

    removed unnecessary directory creation

--- src/AircraftManager/AircraftManager.cpp
+++ src/AircraftManager/AircraftManager.cpp
@@ -7,9 +7,7 @@ AircraftManager::AircraftManager()
     d_dataRoot =
       QStandardPaths::writableLocation(QStandardPaths::HomeLocation) + "/AppData/Roaming/Flight Display Companion";
 
-    QDir dataDirs(QStandardPaths::writableLocation(QStandardPaths::HomeLocation));
-    dataDirs.mkpath("AppData/Roaming/Flight Display Companion/Definitions");
-    dataDirs.mkpath("AppData/Roaming/Flight Display Companion/Thumbnails");
+
 
     QSettings settings;
 
